deaa73a8114449c8c0fa5fe49422a133edb14c0b
alexb000 <145222665+alexb000@users.noreply.github.com>
2024-11-22 14:14:28 -0700

    update platform.cpp

--- EducationalApp/platform.cpp
+++ EducationalApp/platform.cpp
@@ -1,31 +1,22 @@
-#include "view.h"
-#include "view.h"
-#include <QPainter>
+#include "platform.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    // ui->widget->show(); // Unsure what this is
-
-    // for (int i = 0; i < 5; i++) {
-    //     int randXPosition = rand() % 700;
-    //     int randYPosition = rand() % 700;
-    // }
-
-    // Platforms platform(QPoint(500, 500));
-    // platformsList.append(platform);
-
-    // update();
+#include <QDebug>
+Platforms::Platforms(const QPoint& position) {
+    qDebug() << "Inside platform constructor";
+    image = QImage(":/images/log.png");
+    if (image.isNull()) {
+        qDebug() << "Failed to load log.png from resource path.";
+    } else {
+        qDebug() << "Platform image loaded successfully. Size:" << image.size();
+    }
+    boundingRect = QRect(position, image.size());
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+
+QRect Platforms::getBoundingRect(){
+    return boundingRect;
 }
 
-// void MainWindow::paintEvent(QPaintEvent *event) {
-//     QPainter painter(this);
-//     painter.drawImage(platformsList[0].getBoundingRect().topLeft(), platformsList[0].getImage());
-// }
+QImage Platforms::getImage(){
+    return image;
+}
