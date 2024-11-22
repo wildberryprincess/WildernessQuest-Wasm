ccd142b1fbdad48b5671b5dcfa590db659c3cdff
andryjoseph <andrymariajoseph@hotmail.com>
2024-11-22 13:59:07 -0700

    Added our new project and Resources

---
+++ EducationalApp/view.cpp
@@ -0,0 +1,14 @@
+#include "view.h"
+#include "ui_view.h"
+
+View::View(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::View)
+{
+    ui->setupUi(this);
+}
+
+View::~View()
+{
+    delete ui;
+}
