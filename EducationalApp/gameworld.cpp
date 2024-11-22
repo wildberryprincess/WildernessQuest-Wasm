3bfbfdc508c5f823c7dd479046b3601eafacb69a
andryjoseph <andrymariajoseph@hotmail.com>
2024-11-22 15:16:51 -0700

    Randomly drawing 10 platforms on the game. Changed the UI slightly as well.

--- EducationalApp/gameworld.cpp
+++ EducationalApp/gameworld.cpp
@@ -3,18 +3,24 @@
 #include <QDebug>
 #include <QKeyEvent>
 #include <QGraphicsRectItem>
+#include <cstdlib>
+
 GameWorld::GameWorld(QWidget *parent) : QWidget(parent),
     world(b2Vec2(0.0f, 10.0f)),
     timer(this),
     //setting the image
     image(":/Images/hero.png")
 {
-    Platform platform(QPoint(100, 100));
-    platformsList.append(platform);
-    //  // Create the hero and platforms
-    // createPlatform(50, 400, 200, 20); // Example platform
-    // createPlatform(300, 300, 150, 20);
-    // createPlatform(200, 200, 100, 20);
+
+    for (int i = 0; i < 10; i++) {
+        // Platform platform(QPoint(0 + (i * 150), 200));
+        Platform platform(QPoint(rand() % 1300, rand() % 800));
+        platformsList.append(platform);
+    }
+    // Platform platform(QPoint(0, 0));
+    // platformsList.append(platform);
+
+
     // Define the ground body.
     b2BodyDef groundBodyDef;
     groundBodyDef.position.Set(0.0f, 20.0f);
@@ -59,8 +65,13 @@ void GameWorld::paintEvent(QPaintEvent *) {
     float angle = heroBody->GetAngle();
     //    printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
     painter.drawImage((int)(position.x*20), (int)(position.y*20), image);
-    // painter.drawImage(200, 200, image);
-    painter.drawImage(platformsList[0].getBoundingRect().topLeft(), platformsList[0].getImage());
+
+    // Drawing the platforms
+    for (Platform platform : platformsList) {
+        painter.drawImage(platform.getBoundingRect().topLeft(), platform.getImage());
+    }
+    // painter.drawImage(platformsList[0].getBoundingRect().topLeft(), platformsList[0].getImage());
+
     //    qDebug() << image;
     painter.end();
 }
@@ -69,22 +80,3 @@ void GameWorld::updateWorld() {
     world.Step(1.0/60.0, 6, 2);
     update();
 }
-// void GameWorld::createPlatform(float x, float y, float width, float height)
-// {
-//     // Define the platform body
-//     b2BodyDef platformDef;
-//     platformDef.position.Set(x / 30.0f, y / 30.0f); // Convert to meters
-//     b2Body *platformBody = world.CreateBody(&platformDef);
-//     // Define the shape
-//     b2PolygonShape platformShape;
-//     platformShape.SetAsBox(width / 30.0f / 2, height / 30.0f / 2); // Half-width, half-height
-//     // Create the fixture
-//     platformBody->CreateFixture(&platformShape, 0.0f); // Static objects have 0 density
-//     m_platforms.push_back(platformBody);
-//     //TODO: Next task, figure out how to draw the platforms on the widget.
-//     // Add to the scene for visualization
-//     QGraphicsRectItem *platformItem = new QGraphicsRectItem(0, 0, width, height);
-//     platformItem->setBrush(Qt::gray);
-//     platformItem->setPos(x, y);
-//     platformItem->show();
-// }
