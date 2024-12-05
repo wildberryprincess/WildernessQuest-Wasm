a7a0df49c420944b3ba6bdd96f3217d61607aa50
wildberryprincess <kitchenseliza@gmail.com>
2024-12-05 14:09:49 -0700

    style update for progress bar

--- EducationalApp/gameworld.cpp
+++ EducationalApp/gameworld.cpp
@@ -583,13 +583,10 @@ void GameWorld::changeProgressBar(int numAnswered) {
     progressBar->setValue(numAnswered);
 
     numOfQuestionsLeft = 2 - numAnswered;
-<<<<<<< Updated upstream
     progressText = QString::number(numOfQuestionsLeft) + " question to go!";
-=======
     QString progressText = QString::number(numOfQuestionsLeft) + " question to go!";
     QFont courierFont("Courier", 12);
     progressLabel->setFont(courierFont);
->>>>>>> Stashed changes
 
     //QString progressText = QString("%1 questions to go").arg(numOfQuestionsLeft);
     progressLabel->setText(progressText);
@@ -597,11 +594,8 @@ void GameWorld::changeProgressBar(int numAnswered) {
 
 void GameWorld::setProgressBarToZero() {
     progressBar->setValue(0);
-<<<<<<< Updated upstream
     progressLabel->setText("2 questions to go!");
-=======
     QFont courierFont("Courier", 12); // Font: Courier, size: 12
     progressLabel->setFont(courierFont);
     progressLabel->setText("0 questions to go!");
->>>>>>> Stashed changes
 }
